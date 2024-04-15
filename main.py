cb5eb15e8b7bb52f29b606e74d65a03ecbd74432
Mark_Tarulin <evilsobol@gmail.com>
2024-04-15 12:22:47 +0700

    2.3. Получены карты, xlsx файлы по изменненой бд

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ if __name__ == '__main__':
     ex = ExperimentData()
     m = Map()
     upl = ApproximatedMapUploader()
-    ex.get_co_min_equations()
-    print(ex.co_equation_dict)
-    '''
+
+    m.save_all_source_map()
     m.save_all_source_map_to_excel()
+
     m.save_all_rbf_map()
     m.save_all_rbf_map_to_excel()
+
     m.save_all_co_min_map()
     m.save_all_o2_min_map()
-    '''
+
 
 
