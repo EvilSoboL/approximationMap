358b5c1ceff05e21e1383514a8ad65313986727a
EvilSoboL <evilsobol01@gmail.com>
2024-10-18 11:08:22 +0700

    Добавлены результаты по маслу 40 и 60с c интерполяцией значений

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ if __name__ == '__main__':
 
     #db.get_experiment_data('waste_oil_40_2024', 'steam', 'CO')
     #m.save_all_source_map_to_excel()
-    m.save_all_source_map()
+    #m.save_all_source_map()
 
-    m.save_all_rbf_map()
+    #m.save_all_rbf_map()
     #m.save_all_rbf_map_to_excel()
 
-    #m.save_all_co_min_map()
+    m.save_all_co_min_map()
     #m.save_all_o2_min_map()
 
 
